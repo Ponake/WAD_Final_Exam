2fd7e1ba2af02a74b35b2f6498d926fed4dd4110
Ponake <74523071+Ponake@users.noreply.github.com>
2021-03-24 11:36:58 +0700

    Add files via upload

---
+++ create.php
@@ -0,0 +1,52 @@
+<?php
+require 'db.php';
+$message = '';
+if (isset ($_POST['name'])  && isset($_POST['email']) && isset($_POST['surname']) ) {
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $email = $_POST['email'];
+  $sql = 'INSERT INTO people(name, email, surname) VALUES(:name, :email, :surname)';
+  $statement = $connection->prepare($sql);
+  if ($statement->execute([':name' => $name, ':email' => $email, ':surname' => $surname])) {
+    $message = 'data inserted successfully';
+  }
+
+
+
+}
+
+
+ ?>
+<?php require 'header.php'; ?>
+<div class="container">
+  <div class="card mt-5">
+    <div class="card-header">
+      <h2>Create a person</h2>
+    </div>
+    <div class="card-body">
+      <?php if(!empty($message)): ?>
+        <div class="alert alert-success">
+          <?= $message; ?>
+        </div>
+      <?php endif; ?>
+      <form method="post">
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" class="form-control">
+        </div>
+		<div class="form-group">
+          <label for="surname">Surname</label>
+          <input type="text" name="surname" id="surname" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-info">Create a person</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<?php require 'footer.php'; ?>
